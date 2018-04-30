b15911dec17a2901b1fb6311bb93f887cb9b9fdc
Karen Pacheco <pachecovejar.k@gmail.com>
2018-04-30 00:03:58 -0300

    added associationes, post & user

--- lib/ecto_assoc/user.ex
+++ lib/ecto_assoc/user.ex
@@ -5,5 +5,6 @@ defmodule EctoAssoc.User do
     field :name, :string
     field :email, :string
     has_one :avatar, EctoAssoc.Avatar  # added
+    has_many :posts, EctoAssoc.Post  # this was added
   end
 end
